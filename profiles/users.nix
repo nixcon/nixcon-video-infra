464725e85cd45ad85867402696e20c0650ba339a
Alyssa Ross <hi@alyssa.is>
2020-08-18 00:59:26 +0000

    profiles/users: add puck
    Compare with <https://puck.moe/puck.pub>.

--- profiles/users.nix
+++ profiles/users.nix
@@ -7,6 +7,15 @@
   users.mutableUsers = false;
   security.sudo.wheelNeedsPassword = false;
 
+  users.users.puck = {
+    isNormalUser = true;
+    description = "Puck Meerburg";
+    extraGroups = [ "wheel" ];
+    openssh.authorizedKeys.keys = [
+      "cert-authority ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCtxV2PBVLfhMLXBbmEE6x3FwmoiYILf3VbhPFZH3Wnqy7P5JM2g6aIXPMi5GyrL+FuG24Wiz7erl9MIB73eSCIcrEMFcDtch5R19kKtH7OMnC6LPgAxBpZycHmXWAmQ5dmkFH6Exr/+DvQ6OxJk+T7y4K6zhwunjfqlKiPsLL1WVyxuSaj1lnvzRmaPL99slfg5RcXg+sbIEhSVKOsKjbuS88RNPjfAOcGMhFsMHBHPLYoVOgPL263TokyDXlN7/50IbV8b+ul3Zr1f0ntjts9qgjWJGK5D5VAkX7jBkg2H2jM1JN5IbSgrTxFQx9cbzJg4JEFPCv1Oo1gHan8zbzKJrJPWnRv9Gt/UcvmhD2hijnK8FrC36S5/lmCHxDlwCtEfAjL1y7Cdp889AuvYbShNJA29I2iNt7i0p1VluqzjexxvquN7u/M/ftLDdzUzfCVFJzPgp3RDEiv+9yjpwrNcEzgLe2OwSonAt+papkorz3n8gWY5maSlvWntSQx/PahVsAVFN/+7tEUu5F0uKbD7xYKdBRUEFl+/h+AFEMCFuKVxyCrDncvlm9WzTOoCovCymADfJokBnjag1v+8XdS9xFQo/TVrL5T2/FLjVnY89k0rVPlxcf0kaNnshp8SWLTbYXj4zfMxC8UDSw7OrTKa0WOCUrVeEbehR0ezfSUAQ== puck"
+    ];
+  };
+
   users.users.qyliss = {
     isNormalUser = true;
     description = "Alyssa Ross";
@@ -18,5 +27,5 @@
 
   users.users.root.openssh.authorizedKeys.keys =
     lib.concatMap (u: config.users.users.${u}.openssh.authorizedKeys.keys)
-      [ "qyliss" ];
+      [ "puck" "qyliss" ];
 }
