9fa94c945fb511b0338b6e3d88a6de6a7e768f16
Alyssa Ross <hi@alyssa.is>
2020-08-31 15:14:01 +0000

    sys/dash01: init from testcon
    Moving to a Google Cloud instance rather than a NixOS container
    running on one of puck's machines.

--- sys/dash01.nix
+++ sys/dash01.nix
@@ -7,20 +7,15 @@
   imports = [
     ../modules/dash.nix
     ../profiles/base.nix
+    ../profiles/gcloud.nix
   ];
 
-  boot.isContainer = true;
-  networking.useDHCP = false;
-  networking.hostName = "testcon";
-  networking.domain = "puck.moe";
+  networking.hostName = "dash01";
+  networking.domain = "nixcon.net";
 
   # TOOD: set up security@nixcon.net; have that forward to everyone on
   # the team, and then set this globally in acme.nix.
   security.acme.email = "hi@alyssa.is";
 
-  environment.etc."resolv.conf".text = ''
-    nameserver 1.1.1.1
-  '';
-  
   nixcon.dash.enable = true;
 }
