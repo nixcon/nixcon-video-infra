a5bf663efc24d0042b471feef35a44e2110e1048
Alyssa Ross <hi@alyssa.is>
2020-09-22 14:11:13 +0000

    modules/dash: support multiple stream variants
    ffmpeg (rather than nginx) listens for incoming RTMP on port 1935,
    transcodes that into three different resolutions, and then forwards
    each transcoded stream to the nginx listening on port 1936, which then
    produces a DASH stream from its RTMP input.

--- modules/dash.nix
+++ modules/dash.nix
@@ -42,15 +42,28 @@ in
     services.nginx.package = with pkgs;
       nginx.override { modules = with nginxModules; [ rtmp ]; };
 
+    systemd.services.ffmpeg = {
+      serviceConfig.ExecStart =
+        "${pkgs.ffmpeg}/bin/ffmpeg -listen 1 -i rtmp://0.0.0.0:1935/src/main" +
+        " -f flv -c:v libx264 -preset fast -s 854x480 rtmp://localhost:1936/dash/main_480p" +
+        " -f flv -c:v libx264 -preset fast -s 1200x720 rtmp://localhost:1936/dash/main_720p" +
+        " -f flv -c:v libx264 -preset fast -s 1920x1080 rtmp://localhost:1936/dash/main_1080p";
+      serviceConfig.Restart = "always";
+    };
+
     services.nginx.appendConfig = ''
       rtmp {
         server {
-          listen 1935;
+          listen 1936;
 
           application dash {
             live on;
             dash on;
+            dash_nested on;
             dash_path /run/nginx/dash;
+            dash_variant _480p bandwidth="409920" width="854" height="480";
+            dash_variant _720p bandwidth="921600" width="1200" height="720";
+            dash_variant _1080p bandwidth="2073600" width="1920" height="1080" max;
           }
         }
       }
