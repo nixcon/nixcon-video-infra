2bca533276a0d842ce8c71f7112502b75c413102
Puck Meerburg <puck@puck.moe>
2020-10-05 10:39:42 +0000

    modules/dash: configure for low-latency DASH
    This configuration generates 2-second segments, with the keyframe
    interval set to 2 seconds at 30fps (this is synced up with the incoming
    OBS stream), and configuring low-latency DASH both here and on the
    DASH.js client. This brings the latency down from 30 seconds to just
    over 12 (measured from a text file being rendered by OBS).

--- modules/dash.nix
+++ modules/dash.nix
@@ -47,14 +47,15 @@ in
           -c:v:0 libx264 -map v:0 -b:v:0 800k -s:0 854x480 -aspect:0 16:9 -preset:0 fast \
           -c:v:1 libx264 -map v:0 -b:v:1 1400k -s:1 1280x720 -aspect:1 16:9 -preset:1 fast \
           -c:v:2 copy -map v:0 -aspect:2 16:9 \
+          -g 60 \
           -map 0:a \
           -f dash \
               -init_seg_name 'init$RepresentationID$.$ext$' \
               -media_seg_name 'chunk$RepresentationID$-$Number%05d$.$ext$' \
               -use_template 1 -use_timeline 1 \
-              -seg_duration 5 -window_size 20 -remove_at_exit 1 \
+              -seg_duration 2 -window_size 20 -remove_at_exit 1 \
               -hls_playlist 1 \
-              -streaming 1 \
+              -streaming 1 -ldash 1 \
               -adaptation_sets "id=0,streams=v id=1,streams=a" \
               /run/nginx/dash/main.mpd
       '';
